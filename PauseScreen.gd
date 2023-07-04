f5df448debc85a08bcfaf1860fc3dd799251e3ba
Jason <45276567+MathedPotato@users.noreply.github.com>
2023-07-04 15:32:13 +1000

    Convert pausedState to enum

--- PauseScreen.gd
+++ PauseScreen.gd
@@ -2,29 +2,35 @@ extends ColorRect
 
 @export var pausedColour : Color
 @export var unpausedColour : Color
-var pausedState := 0
+
+enum PausedStates {
+	UNPAUSED,
+	PAUSED,
+	PAUSING,
+	UNPAUSING
+}
+var pausedState := PausedStates.UNPAUSED
 
 
 func process(delta):
-	if pausedState >= 2:
-		if pausedState == 2:
-			color = pausedColour
-			pausedState = 1
-		else:
-			color = unpausedColour
-			pausedState = 0
-			StateStack.PopStack()
+	if pausedState == PausedStates.PAUSING:
+		color = pausedColour
+		pausedState = PausedStates.PAUSED
+	elif pausedState == PausedStates.UNPAUSING:
+		color = unpausedColour
+		pausedState = PausedStates.UNPAUSED
+		StateStack.PopStack()
 
 
 func input(event: InputEvent):
 	if event.is_action_pressed("Pause"):
-		if pausedState < 2:
+		if pausedState < PausedStates.PAUSING:
 			TogglePause()
 
 
 func TogglePause():
-	if pausedState == 0:
-		pausedState = 2
+	if pausedState == PausedStates.UNPAUSED:
+		pausedState = PausedStates.PAUSING
 		StateStack.PushStack(self)
-	elif pausedState == 1:
-		pausedState = 3
+	elif pausedState == PausedStates.PAUSED:
+		pausedState = PausedStates.UNPAUSING
