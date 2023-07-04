b5736884e40a2b0497baa28944c2a24b83b21d5c
Jason <45276567+MathedPotato@users.noreply.github.com>
2023-07-04 16:24:37 +1000

    Add script template for state stack objects

--- StateStack.gd
+++ StateStack.gd
@@ -1,5 +1,13 @@
 extends Node
-
+# This is the main state stack object that runs on game launch as a singleton.
+# It essentially relays built-in function calls (_process, _physics_process and _input)
+#  to the object at the top of the stack (usually a scene root node, which then relays
+#  those same function calls to it's children), calling that object's process, physics_process
+#  and input functions (note the absence of the underscore prefix).
+# For this to work properly, this does rely on making sure all objects (with possible exceptions)
+#  use the non-underscore versions of those functions (making a script template helps with this).
+# The state stack proves to be useful in managing the active state of groups and subgroups of
+#  objects, and is particularly useful when implementing a pause screen.
 
 var stack : Array
 
@@ -11,12 +19,14 @@ func _process(delta: float) -> void:
 	if stack.back().has_method("inactive_process"):
 		stack.back().inactive_process(delta)
 
+
 func _physics_process(delta: float) -> void:
 	if stack.size() == 0: return
 	if stack.back().has_method("physics_process"):
 		stack.back().physics_process(delta)
 	if stack.back().has_method("inactive_physics_process"):
 		stack.back().inactive_physics_process(delta)
+
 
 func _input(event: InputEvent) -> void:
 	if stack.size() == 0: return
@@ -25,6 +35,7 @@ func _input(event: InputEvent) -> void:
 	if stack.back().has_method("inactive_input"):
 		stack.back().inactive_input(event)
 
+
 func PushStack(state: Node) -> void:
 	if stack.size() > 0 and stack.back().has_method("deactivate_state"):
 		stack.back().deactivate_state()
@@ -32,6 +43,7 @@ func PushStack(state: Node) -> void:
 #	state.tree_exiting.connect(PopStack)
 	if stack.back().has_method("activate_state"):
 		stack.back().activate_state()
+
 
 func PopStack() -> Node:
 	var topState = stack.pop_back()
---
+++ script_templates/Node/StateStackObject.gd
@@ -0,0 +1,17 @@
+# meta-default: true
+# meta-description: Base template for an object that will be controlled under a state stack
+
+extends _BASE_
+
+
+func _ready() -> void:
+	pass
+
+func process(delta: float) -> void:
+	pass
+
+func physicsProcess(delta: float) -> void:
+	pass
+
+func input(event: InputEvent) -> void:
+	pass
